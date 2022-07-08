fc119a1f88e31de86bab0e6bb686791de64d2554
Ryan <ryanrasti@protonmail.com>
2022-07-08 15:29:49 -0700

    fix an issue

--- pulumi.py
+++ pulumi.py
@@ -1,18 +1,12 @@
 import os
 import sys
 
-from rules_python.python.runfiles import runfiles
-
-# BASE_DIR (in brackets) is substituted as a genrule:
-BASE_DIR = os.path.relpath('{{BASE_DIR}}', 'bazel-out/host/bin')
-
 def path(p):
-  r = runfiles.Create()
-  return os.path.realpath(r.Rlocation(os.path.join('pulumi_bazel', BASE_DIR, p)))
+  return os.path.realpath((os.path.join(os.path.dirname(__file__), p)))
 
 def main():
-  os.environ['PULUMI_PYTHON_CMD'] = path('python')
-  os.chdir(os.path.dirname(path('Pulumi.yaml')))
+  os.environ['PULUMI_PYTHON_CMD'] = path('./python')
+  os.chdir(path('.'))
   args = ['pulumi'] + sys.argv[1:]
   os.execvp(args[0], args)
 
