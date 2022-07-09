cd85f847553f2e5b80ff214295cbccb998e44e2d
Ryan <ryanrasti@protonmail.com>
2022-07-08 18:02:48 -0700

    local test

--- env/run_tests.sh
+++ env/run_tests.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-set -e
+set -ev
+
+REPO=$(git rev-parse --show-toplevel)
+TMP=$(mktemp -d)
+echo "In $TMP"
+cd "$TMP"
+git clone "$REPO"
+cd $(basename "$REPO")
 
 TAG=tester:local-$(uuidgen)
-docker build . -t $TAG
-docker run -it -v $(pwd)/..:/src $TAG test/test.sh
+docker build env/. -t $TAG
+docker run -it -v $(pwd):/src $TAG test/test.sh
